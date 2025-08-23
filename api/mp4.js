a8417e97e8e299b75af60f4a1af76a4902c69fcc
usman82921 <usman05054@gmail.com>
2025-08-23 08:08:08 +0500

    Update mp4.js

--- api/mp4.js
+++ api/mp4.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       res.status(200).json({
         success: true,
         title: data.data.name_mp4,
-        download: data.data.url_mp4_youtube  // 👈 direct signed link
+        download: data.data.url_mp4_youtube   // fresh direct link
       });
     } else {
       res.status(200).json({ success: false });
